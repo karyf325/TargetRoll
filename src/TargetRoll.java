275f5501fe5032f74353d156370b068448fcf87d
karyf325 <43179791+karyf325@users.noreply.github.com>
2018-12-18 12:42:01 -0500

    Add files via upload

---
+++ src/TargetRoll.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class TargetRoll {
+
+	public static void main(String[] args) {
+	 
+		Random x = new Random();
+		
+		System.out.println("Please enter a number from 1 to 6: ");
+		int num = x.nextInt(); 
+		
+		
+		while (num > 1 );
+		{
+			System.out.println("Correct");
+			
+		}
+		
+	
+		
+		
+		
+		
+		
+		
+	
+}
+
+		
+	}
